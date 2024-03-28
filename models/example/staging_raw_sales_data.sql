540d625640b828cdccc7ff9928c38f59a93a1bde
Shrikant <shrikantraut780@gmail.com>
2024-03-28 11:41:43 +0000

    Commit changes

---
+++ models/example/staging_raw_sales_data.sql
@@ -0,0 +1,13 @@
+
+
+-- Create staging model for raw sales data
+SELECT
+    order_id,
+    customer_id,
+    CAST(order_date AS DATE) AS order_date,
+    product_id,
+    quantity,
+    CAST(unit_price AS DECIMAL(10,2)) AS unit_price,
+    CAST(total_price AS DECIMAL(10,2)) AS total_price
+FROM
+    {{ ref('raw_sales_data') }};
