9f131bb0c0c9815535c5f70463502223ab087a79
Shrikant <shrikantraut780@gmail.com>
2024-03-28 12:00:05 +0000

    commit

---
+++ models/example/staging_raw_customer_data.sql
@@ -0,0 +1,12 @@
+-- models/staging/raw_customer_data.sql
+
+CREATE OR REPLACE VIEW staging_customer_data AS
+SELECT
+    customer_id,
+    first_name,
+    last_name,
+    email,
+    phone_number,
+    TO_DATE(registration_date, 'YYYY-MM-DD') AS registration_date
+FROM
+    raw_customer_data;
