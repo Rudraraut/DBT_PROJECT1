89aabfd9a5cf5d2cba4626b18415ad717db9ae0d
Shrikant Raut <shrikantraut780@gmail.com>
2024-04-01 09:50:54 +0000

    commit changes

--- models/example/raw_sales_data.sql
+++ models/example/raw_sales_data.sql
@@ -1,12 +1,14 @@
-
--- Create staging model for raw sales data
-SELECT
-    order_id,
-    customer_id,
-    CAST(order_date AS DATE) AS order_date,
-    product_id,
-    quantity,
-    CAST(unit_price AS DECIMAL(10,2)) AS unit_price,
-    CAST(total_price AS DECIMAL(10,2)) AS total_price
-FROM
-    {{ ref('raw_sales_data') }};
+create or replace view DEMO_DB.TEST_SCHEMA_TEST_SCHEMA.raw_sales_data
+as (
+    
+    SELECT
+        order_id,
+        customer_id,
+        CAST(order_date AS DATE) AS order_date,
+        product_id,
+        quantity,
+        CAST(unit_price AS DECIMAL(10,2)) AS unit_price,
+        CAST(total_price AS DECIMAL(10,2)) AS total_price
+    FROM
+        DEMO_DB.TEST_SCHEMA.raw_sales_data
+);
