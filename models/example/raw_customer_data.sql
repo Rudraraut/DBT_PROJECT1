1be09c2ffa41dc4f48a3f7d04a830fc020be9335
Shrikant Raut <shrikantraut780@gmail.com>
2024-04-01 11:30:33 +0000

    commit

--- models/example/raw_customer_data.sql
+++ models/example/raw_customer_data.sql
@@ -1,10 +1,5 @@
--- Declare dependency on the staging model
-{{ config(
-    materialized='table',
-) }}
-
 -- Create or replace view for raw customer data
-CREATE OR REPLACE VIEW {{ ref('raw_customer_data') }} AS
+CREATE OR REPLACE VIEW raw_customer_data AS
 SELECT
     customer_id,
     first_name,
@@ -13,4 +8,4 @@ SELECT
     phone_number,
     registration_date
 FROM
-    {{ ref('stg_raw_customer_data') }};
+    stg_raw_customer_data;
