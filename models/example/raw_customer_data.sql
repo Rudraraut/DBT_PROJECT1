6a7f58a4f171ef9d4c3cfa92dbe5570052e950c9
Shrikant Raut <shrikantraut780@gmail.com>
2024-04-01 09:45:29 +0000

    commit

--- models/example/raw_customer_data.sql
+++ models/example/raw_customer_data.sql
@@ -1,9 +1,13 @@
-SELECT
-    customer_id,
-    first_name,
-    last_name,
-    email,
-    phone_number,
-    TO_DATE(registration_date, 'YYYY-MM-DD') AS registration_date
-FROM
-    {{ source('raw_customer_data', 'raw_customer_data') }};
+create or replace transient table DEMO_DB.TEST_SCHEMA_TEST_SCHEMA.raw_customer_data
+as
+select * from (
+    select
+        customer_id,
+        first_name,
+        last_name,
+        email,
+        phone_number,
+        TO_DATE(registration_date, 'YYYY-MM-DD') AS registration_date
+    from
+        DEMO_DB.TEST_SCHEMA.raw_customer_data
+);
