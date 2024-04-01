01c43edd2b9f7c54ec013fc9a34cdec101ea68f6
Shrikant Raut <shrikantraut780@gmail.com>
2024-04-01 11:16:06 +0000

    commit

--- models/example/raw_customer_data.sql
+++ models/example/raw_customer_data.sql
@@ -6,4 +6,14 @@
     depends_on=[ref('stg_raw_customer_data')]
 ) }}
 
--
+-- Create or replace view for raw customer data
+CREATE OR REPLACE VIEW {{ ref('raw_customer_data') }} AS
+SELECT
+    customer_id,
+    first_name,
+    last_name,
+    email,
+    phone_number,
+    registration_date
+FROM
+    {{ ref('stg_raw_customer_data') }};
