e78153e08777555a10f5f0490332d94a7ebb48b2
Shrikant Raut <shrikantraut780@gmail.com>
2024-04-01 11:33:57 +0000

    commit

--- models/example/raw_customer_data.sql
+++ models/example/raw_customer_data.sql
@@ -1,5 +1,5 @@
 -- Create or replace view for raw customer data
-CREATE OR REPLACE VIEW raw_customer_data AS
+CREATE OR REPLACE VIEW DEMO_DB.TEST_SCHEMA_TEST_SCHEMA.raw_customer_data AS
 SELECT
     customer_id,
     first_name,
